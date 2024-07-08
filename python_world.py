5646935866e1fba14898f41d27cf3ccfd47da6a0
kev.hernandezgarza@gmail.com <kevinhernandez@Kevins-MacBook-Air.local>
2024-07-07 21:53:18 -0500

    learning about variables

--- python_world.py
+++ python_world.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+# declaring variables
+message = "Hello World!"
+print(message)
+
+name = "My name is Kevin"
+print(name)
+
+# using methods
+# this methods are actions acted upon a variable name
+group_name = "tHe avEngERs"
+
+# title methods capitalizes every first word
+print(group_name.title())
+
+# uppercase all letters
+print(group_name.upper())
+
+# lowercases all letter
+print(group_name.lower())
