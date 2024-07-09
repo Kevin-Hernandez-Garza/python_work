af7e0026a9eac825742fcc5b4fa4277b9ea5f784
kev.hernandezgarza@gmail.com <kevinhernandez@Kevins-MacBook-Air.local>
2024-07-08 21:21:21 -0500

    learning f strings

--- python_world.py
+++ python_world.py
@@ -17,3 +17,12 @@ print(group_name.upper())
 
 # lowercases all letter
 print(group_name.lower())
+
+# using f string, f stands for format string 
+first_name = "kevin"
+last_name = "hernandez" 
+full_name = f"{first_name} {last_name}"
+# format the full_name capitalzing the first letter of the first & last name
+message = f"Hello my name is {full_name.title()}!"
+# prints out name
+print(message)
