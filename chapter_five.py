47e3fbf9b62b9ee70975a87ed6f4ef1612bd3dd9
kev.hernandezgarza@gmail.com <kev.hernandezgarza@gmail.com>
2024-08-01 23:01:44 -0500

    learning about inequality and comparison expressions

--- chapter_five.py
+++ chapter_five.py
@@ -10,21 +10,34 @@ for car in cars:
         print(car.title())
 
 
-# conditional test
+# conditional expression
 # returns a true or false output
 name = "kevin"
 print(f"\n\n{name=="kevin"}\n")
 print(name=="pacman")
 
-# conditional tests are case sensitive
+# conditional expression are case sensitive
 team = "Chelsea"
 # returns false
 print(f"\n{team=="chelsea"}\n")
 # returns true 
 print(team.lower()=="chelsea")
 
+# inequality expression
+favorite_game = "pacman"
+# this will print the message since it doesn't equal the value
+if favorite_game != "superman":
+    print("You don't know my favorite game!")
 
 
-
+# numerical comparison
+print("\n\n")
+age = 29
+# prints true
+print(age== 29)
+# numerical comparison using the inequality operator
+# the test passes the condition as it does not equal 40
+if age != 40:
+    print("You are not old enough yet!")
 
 
