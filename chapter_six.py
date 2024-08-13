4d496ea51695a624bd84db83b589cbc160ac9ac8
kev.hernandezgarza@gmail.com <kev.hernandezgarza@gmail.com>
2024-08-12 22:24:09 -0500

    modifying dictionaries and learning their best use cases

--- chapter_six.py
+++ chapter_six.py
@@ -15,3 +15,18 @@ person['favorite food'] = 'wings'
 # printing new person dictionary
 print(person)
 
+# Starting with an empty Dictionary
+# adding key/value pairs as we go
+city_state = {}
+city_state["Colorado"] = "Denver"
+city_state["Hawaii"] = "Oahu"
+print(city_state)
+
+
+# modifying values in a dictionary 
+print(f"\n\nThe city chosen in Hawaii is {city_state["Hawaii"]}")
+# chaging the value of hawaii
+city_state["Hawaii"] = "Kona"
+print(f"The new value for Hawaii is {city_state["Hawaii"]}")
+
+
