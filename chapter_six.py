4a1373cb5bf47e4a4edf843cbe3aa6abfeefd19a
kev.hernandezgarza@gmail.com <kev.hernandezgarza@gmail.com>
2024-08-13 23:37:06 -0500

    learning how to delete keys and reinforcing what I learned

--- chapter_six.py
+++ chapter_six.py
@@ -31,7 +31,7 @@ print(f"The new value for Hawaii is {city_state["Hawaii"]}")
 
 
 # small program that determines the amount of points earned 
-user_0 = {'points': 0, 'luck' : 'extremely'}
+user_0 = {'points': 0, 'luck' : 'extremely', "color" : "red"}
 print(f"\nYour current point value is {user_0['points']}")
 
 if user_0['luck'] == 'low':
@@ -45,3 +45,74 @@ user_0['points'] = user_0['points'] + points
 print(f"\nYour new point value is {user_0['points']}\n")
 
 
+# deleting key/value pairs
+print(f"\n{user_0}")
+# using the del keyword to delete key/value pair
+# removes it permanently 
+del user_0["color"]
+print(user_0)
+
+
+# another dictionary 
+# this time about on a poll of people's favorite programming languages
+favorite_languages = {
+    'sarah' : 'c++', 
+    'kevin' : 'python', 
+    'scott' : 'java', 
+    'john' : 'c',
+    'steven' : 'python', 
+    }
+# getting kevin's favorite language and assigning it a value 
+# using the title method to capitalize first letter
+language = favorite_languages['kevin'].title()
+print(f"\nKevin's favorite programming language is {language}\n")
+
+
+# using the get method in case the key/value pair does not exist
+# instead of getting an error message back while using the [] brackets. 
+print(favorite_languages.get('michelle', 'Michelle does not exist!'))
+
+
+# EXERCISES
+
+# 6-1 Person
+developer = {
+    'first_name' : 'kevin',
+    'last_name' : 'hernandez',
+    'age' : 29,
+    'city' : 'austin',
+    }
+
+print(f"\nThe developer's first name is {developer['first_name'].title()}!\n")
+print(f"\nThe developer's first name is {developer['last_name'].title()}!\n")
+print(f"\nThe developer's age is {developer['age']}\n")
+print(f"\nThe developer's place of birth is {developer['city'].title()}!\n")
+
+
+# 6-2 Favorite Numbers
+friends_numbers = {
+    'dobby' : 8, 
+    'duncan' : 8, 
+    'chrys' : 14,
+    'kevin' : 4,
+    }
+
+print(f"Dobby's favorite number is {friends_numbers['dobby']}")
+print(f"Duncan's favorite number is {friends_numbers['duncan']}")
+print(f"Chrys's favorite number is {friends_numbers['chrys']}")
+print(f"Kevin's favorite number is {friends_numbers['kevin']}\n")
+
+
+# 6-3 Glossary
+glossary = {
+    'variable' : 'Reserved memory location to store values', 
+    'dictionary' : 'Mutable DS to store key/value pairs', 
+    'if statement' : 'Conditional to check conditions', 
+    'for loop' : 'Used to repeateadly prints a set of statements',
+    }
+
+print("\nDictionary\n")
+print(f"Variable:\n{glossary['variable']}\n")
+print(f"Dictionaries:\n{glossary['dictionary']}\n")
+print(f"If Statement:\n{glossary['if statement']}\n")
+print(f"For Loop:\n{glossary['for loop']}\n\n")
