374ed0a2f3ee751db2db16588f3b65661d48691a
kev.hernandezgarza@gmail.com <kev.hernandezgarza@gmail.com>
2024-09-03 17:37:32 -0500

    function that takes in user input

--- chapter_eight.py
+++ chapter_eight.py
@@ -6,3 +6,13 @@ def greeting():
     print("Hello, hope you're having a great day!")
 # calling the function
 greeting()
+
+
+# function that passes information into it
+# declaring a username variable which accepts an user input 
+username = input("Enter your name here: ")
+# defining the function which accepts a username variable
+def user_greeting(username):
+    print(f"Hello, {username.title()}!")
+# calling function
+user_greeting(username)
