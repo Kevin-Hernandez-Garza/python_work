bb187b998a3b9367169257dd0b62572fc2a471c3
kev.hernandezgarza@gmail.com <kev.hernandezgarza@gmail.com>
2024-09-04 23:39:27 -0500

    learning about positional arguments in functions

--- chapter_eight.py
+++ chapter_eight.py
@@ -1,35 +1,48 @@
-# Functions
+# # Functions
 
-# simple function
-def greeting():
-    """Greeting the user"""
-    print("Hello, hope you're having a great day!")
-# calling the function
-greeting()
-
-
-# function that passes information into it
-# declaring a username variable which accepts an user input 
-username = input("Enter your name here: ")
-# defining the function which accepts a parameter
-def user_greeting(username):
-    print(f"Hello, {username.title()}!")
-# calling function with the username argument
-user_greeting(username)
+# # simple function
+# def greeting():
+#     """Greeting the user"""
+#     print("Hello, hope you're having a great day!")
+# # calling the function
+# greeting()
 
 
-# EXERCISES
-# 8-1 Message 
-def message():
-    """This function display a simple message 
-    with a brief description of what I am learning"""
-    print("\nIn chapter 8 I am learning about functions\n")
-message()
+# # function that passes information into it
+# # declaring a username variable which accepts an user input 
+# username = input("Enter your name here: ")
+# # defining the function which accepts a parameter
+# def user_greeting(username):
+#     print(f"Hello, {username.title()}!")
+# # calling function with the username argument
+# user_greeting(username)
 
 
-# 8-2 Favorite Book
-book = input("What is your favorite book: ")
-def fav_book(book):
-    """This function asks the user about their favorite book"""
-    print(f"\nI love that your favorite book is {book.title()}\n")
-fav_book(book)
+# # EXERCISES
+# # 8-1 Message 
+# def message():
+#     """This function display a simple message 
+#     with a brief description of what I am learning"""
+#     print("\nIn chapter 8 I am learning about functions\n")
+# message()
+
+
+# # 8-2 Favorite Book
+# book = input("What is your favorite book: ")
+# def fav_book(book):
+#     """This function asks the user about their favorite book"""
+#     print(f"\nI love that your favorite book is {book.title()}\n")
+# fav_book(book)
+
+
+# PASSING ARGUMENTS 
+# positional: when function definitions pass arguments
+# and are called in order they are called
+name = input("What is your name: ")
+profession = input("What is your profession: ")
+# function definition
+def career(name, profession):
+    print(f"Hello, {name.title()}!")
+    print(f"Glad you pursued a profession in {profession.title()}")
+# function call
+career(name, profession)
